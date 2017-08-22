75c60366d96de767d3273eae3566cea246f96e71
Alvadesh <30986311+Alvadesh@users.noreply.github.com>
2017-08-22 13:34:20 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost","root","","vote");
+$con=mysqli_connect("localhost","admin","255075ME","ebdb");
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
